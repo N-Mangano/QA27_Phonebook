721a6a437808baa3e6d64ee03549fa5280b5594e
N-Mangano <140650659+N-Mangano@users.noreply.github.com>
2024-01-21 21:47:15 +0100

    add new files

--- src/main/java/com/phonebook/fw/ApplicationManager.java
+++ src/main/java/com/phonebook/fw/ApplicationManager.java
@@ -4,18 +4,33 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class ApplicationManager {
+  String browser;
 
   WebDriver driver;
 
   UserHelper user;
+
   ContactHelper contact;
+
   HomePageHelper homepage;
 
+  public ApplicationManager(String browser) {
+    this.browser = browser;
+  }
+
   public void init() {
-    driver = new ChromeDriver();
-    driver.get("https://telranedu.web.app");
+    if (browser.equalsIgnoreCase("chrome")) {
+        driver = new ChromeDriver();
+    } else if (browser.equalsIgnoreCase("firefox")) {
+        driver = new FirefoxDriver();
+    } else if (browser.equalsIgnoreCase("edge")) {
+        driver = new EdgeDriver();
+    }
+        driver.get("https://telranedu.web.app");
 
     driver.manage().window().maximize();
     driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
--- src/test/java/com/phonebook/tests/TestBase.java
+++ src/test/java/com/phonebook/tests/TestBase.java
@@ -1,23 +1,35 @@
 package com.phonebook.tests;
 
 import com.phonebook.fw.ApplicationManager;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
+import org.openqa.selenium.remote.Browser;
+import org.testng.annotations.*;
 
 public class TestBase {
 
-  protected static ApplicationManager app = new ApplicationManager();
+  protected static ApplicationManager app = new ApplicationManager(System.getProperty("browser", Browser.CHROME.browserName()));
 
+  //@BeforeMethod
   @BeforeSuite
   public void setUp() {
     app.init();
+    //     System.out.println("Before suite");
   }
 
-  @AfterSuite(enabled = false)
+//    @BeforeTest
+//    public void beforeTest() {
+//        System.out.println("Before test");
+//    }
+//
+//    @AfterTest
+//    public void afterTest() {
+//        System.out.println("After test");
+//    }
+
+  //@AfterMethod
+  @AfterSuite(enabled = true)
   public void tearDown() {
     app.stop();
+    //   System.out.println("After suite");
   }
 
 }
